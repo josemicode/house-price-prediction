48351c8d4551e2fa9fd3b1ab16976cbbb14c2fe1
josemicode <jmiguelojedagutierrez@gmail.com>
2024-06-28 14:34:22 +0200

    variable scaler

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 # Reads the Boston house prices csv
 data = pd.read_csv('boston.csv')
@@ -10,3 +11,9 @@ data.dropna(inplace=True)
 # This method creates a binary column for each categorical variable (1 if the value corresponds, 0 if else)
 # The One-Hot aproach is used so that the machine learning algorithm can access numerical data
 data = pd.get_dummies(data)
+
+# Next is Normalization for data scaling
+# This is important so that features with broader ranges of values do not contribute more than others to the model
+scaler = StandardScaler()
+sc_data = scaler.fit_transform(data)
+
