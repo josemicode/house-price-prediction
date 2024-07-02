c74ee0227c4054a958be6cb865fd2375ce7c778d
josemicode <jmiguelojedagutierrez@gmail.com>
2024-07-02 19:50:37 +0200

    MEDV distribution

--- eda.py
+++ eda.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Reload for Exploratory Data Analysis
 data = pd.read_csv('boston.csv')
@@ -8,3 +10,11 @@ print(data.head())
 print(data.info())
 print(data.describe())
 print(data.isnull().sum())
+
+# Median Value [MEDV] distribution visualized
+plt.figure(1, figsize=(6, 4))
+plt.title("MEDV")
+plt.xlabel("Median Value")
+plt.ylabel("Frequency")
+sns.histplot(data['MEDV'], kde=True)
+plt.show()
