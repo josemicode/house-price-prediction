49e6c5d6022a17960ca26b4125e754518428a196
josemicode <jmiguelojedagutierrez@gmail.com>
2024-07-05 12:29:21 +0200

    working model_handler

--- model_handler.py
+++ model_handler.py
@@ -3,15 +3,17 @@ import joblib
 
 class ModelHandler:
     def __init__(self):
-        folder_path = '/models'
+        folder_path = 'models'
         folder_walk = os.walk(folder_path)
         self.model = any
+        self.filled = False
         try:
             _, _, files = next(folder_walk)
             if files:
                 first = files[0]
                 model_path = os.path.join(folder_path, first)
                 self.model = joblib.load(model_path)
+                self.filled = True
             else:
                 print(f"No files found in {folder_path}")
         except StopIteration:
@@ -23,6 +25,9 @@ class ModelHandler:
 if __name__ == "__main__":
     handler = ModelHandler()
     # Test:
-    features = [0.025, 10, 2, 0, 0.5, 5, 45, 5, 2, 250, 15, 350, 5, 25]
-    prediction = handler.predict(features)
-    print(f'Predicted MEDV: {prediction[0]}')
+    if(handler.filled):
+        features = [0.025, 10, 2, 0, 0.5, 5, 45, 5, 2, 250, 15, 350, 5]
+        prediction = handler.predict(features)
+        print(f'Predicted MEDV: {prediction[0]}')
+    else:
+        print("Please, fill the model list first")
